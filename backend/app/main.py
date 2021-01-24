17c7cc590fcfeb1f690e205656d004d97ee93296
Jitse-Jan <code@jitsejan.com>
2021-01-24 00:16:22 +0000

    Added CORS to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from app.api import books, healthcheck
 from app.db import database, engine, metadata
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 metadata.create_all(engine)
 app = FastAPI()
+origins = [
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
